b369764b007c4570bb12e777fc68c09e3975c454
Troyano97 <ivanferreyradaniel@gmail.com>
2023-09-12 00:43:33 -0300

    se agrego el tablero y base css

--- script.js
+++ script.js
@@ -1,11 +1,48 @@
+function crearJuego() {
+   let selectDificutad = document.getElementById("dificultad");
+   let dificultad = selectDificutad.value;
 
-const matriz = []
+   switch (dificultad) {
+      case "facil":
+         filas = 8;
+         columnas = 8;
+         break;
+      case "intermedio":
+         filas = 12;
+         columnas = 12;
+         break;
+      case "experto":
+         filas = 16;
+         columnas = 16;
+         break;
+      default:
+         filas = 8;
+         columnas = 8;
+         break;
+   }
 
 
-function crearjuego() {
-   
-   let dificultad = parseInt(dificultad.value);
+   let matrizJuego = [];
+   for (let i = 0; i < filas; i++) {
+      matrizJuego.push([]);
+      for (let j = 0; j < columnas; j++) {
+         matrizJuego[i].push(0);
+      }
+   }
 
-console.log ="hola"
 
+   let contenidoHTML = '';
+
+   for (let i = 0; i < filas; i++) {
+      contenidoHTML += '<div class="row">';
+
+
+      for (let j = 0; j < columnas; j++) {
+         contenidoHTML += '<div class="col casilla"></div>';
+
+      }
+      contenidoHTML += '</div>';
+   }
+
+   document.getElementById('cont-juego').innerHTML = contenidoHTML;
 }
