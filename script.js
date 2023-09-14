d45803258747168293ac7937acd169b53d146796
Troyano97 <ivanferreyradaniel@gmail.com>
2023-09-13 23:01:06 -0300

    se agregaron los id a las casilas

--- script.js
+++ script.js
@@ -3,23 +3,27 @@ function crearJuego() {
    let selectDificutad = document.getElementById("dificultad");
    let dificultad = selectDificutad.value;
 
-   let filas, columnas;
+   let filas, columnas,cantidadMinas;
    switch (dificultad) {
       case "facil":
          filas = 8;
          columnas = 8;
+         cantidadMinas = 10;
          break;
       case "intermedio":
          filas = 12;
          columnas = 12;
+         cantidadMinas = 40;
          break;
       case "experto":
          filas = 16;
          columnas = 16;
+         cantidadMinas= 99;
          break;
       default:
          filas = 8;
          columnas = 8;
+         cantidadMinas = 10;
          break;
    }
 
@@ -32,14 +36,20 @@ function crearJuego() {
       }
    }
 
+   const totalCasillas = filas * columnas;
+   const minas = [];
+
+
    //crea el html para la interfaz del juego
    let contenidoHTML = '';
+   let contador = 0;
    for (let i = 0; i < filas; i++) {
       contenidoHTML += '<div class="row">';
 
 
       for (let j = 0; j < columnas; j++) {
-         contenidoHTML += '<div class="col casilla"></div>';
+         const casillaID = contador++;
+         contenidoHTML += `<div id=casilla-"${casillaID}" class="col casilla"></div>`;
 
       }
       contenidoHTML += '</div>';
